22da805ee678d6956be4b102b30e00c81787d21e
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-03-25 18:07:56 +0100

    added Calendar to CalendarScreen.js component with global styles

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import moment from 'moment';
 import { Navbar } from '../ui/Navbar';
+
+// Setup the localizer by providing the moment (or globalize, or Luxon) Object
+// to the correct localizer.
+const localizer = momentLocalizer(moment); // or globalizeLocalizer
+// mock de myEventsList
+const events = [
+  {
+    title: 'Mi tarea',
+    start: moment().toDate(),
+    end: moment()
+      .add(2, 'hours')
+      .toDate(),
+    bgcolor: '#fafafa'
+  }
+];
 
 export function CalendarScreen() {
   return (
-    <div>
+    <div className="calendar-screen">
       <Navbar />
       <h1>CalendarScreen</h1>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+      />
     </div>
   );
 }
