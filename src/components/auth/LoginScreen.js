77198963471b8a3b69f59a46c30709e51f172272
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-03-30 18:31:09 +0200

    startLigin actioncreator & dispatch from loginScreen

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,19 +1,50 @@
 import React from 'react';
 // eslint-disable-next-line import/no-unresolved
 import './login.css';
+import { useDispatch } from 'react-redux';
+import { useForm } from '../../hooks/useForm';
+import { StartLogin } from '../../redux/actioncreators/auth.actioncreator';
 
 export function LoginScreen() {
+  const dispatch = useDispatch();
+  const [formLoginValues, handleLoginInputChange] = useForm({
+    logEmail: 'manu@manu.com',
+    logPassw: '123456'
+  });
+  const { logEmail, logPassw } = formLoginValues;
+
+  const [formRegisterValues, handleRegisterInputChange] = useForm({
+    RegName: 'Pepito Floro',
+    regEmail: 'pepito@email.com',
+    regPassw1: '123456',
+    regPassw2: '123456'
+  });
+
+  const { RegName, regEmail, regPassw1, regPassw2 } = formRegisterValues;
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    dispatch(StartLogin(logEmail, logPassw));
+  };
+
+  const handleRegisterSubmit = (e) => {
+    e.preventDefault();
+    console.log(formRegisterValues);
+  };
+
   return (
     <div className="container login-container">
       <div className="row">
         <div className="col-md-6 login-form-1">
           <h3>Ingreso</h3>
-          <form>
+          <form onSubmit={handleLoginSubmit}>
             <div className="form-group">
               <input
                 type="text"
                 className="form-control"
                 placeholder="Correo"
+                name="logEmail"
+                value={logEmail}
+                onChange={handleLoginInputChange}
               />
             </div>
             <div className="form-group">
@@ -21,6 +52,9 @@ export function LoginScreen() {
                 type="password"
                 className="form-control"
                 placeholder="Contraseña"
+                name="logPassw"
+                value={logPassw}
+                onChange={handleLoginInputChange}
               />
             </div>
             <div className="form-group">
@@ -31,12 +65,15 @@ export function LoginScreen() {
 
         <div className="col-md-6 login-form-2">
           <h3>Registro</h3>
-          <form>
+          <form onSubmit={handleRegisterSubmit}>
             <div className="form-group">
               <input
                 type="text"
                 className="form-control"
                 placeholder="Nombre"
+                name="RegName"
+                value={RegName}
+                onChange={handleRegisterInputChange}
               />
             </div>
             <div className="form-group">
@@ -44,6 +81,9 @@ export function LoginScreen() {
                 type="email"
                 className="form-control"
                 placeholder="Correo"
+                name="RegEmail"
+                value={regEmail}
+                onChange={handleRegisterInputChange}
               />
             </div>
             <div className="form-group">
@@ -51,6 +91,9 @@ export function LoginScreen() {
                 type="password"
                 className="form-control"
                 placeholder="Contraseña"
+                name="regPassw1"
+                value={regPassw1}
+                onChange={handleRegisterInputChange}
               />
             </div>
 
@@ -59,6 +102,9 @@ export function LoginScreen() {
                 type="password"
                 className="form-control"
                 placeholder="Repita la contraseña"
+                name="regPassw2"
+                value={regPassw2}
+                onChange={handleRegisterInputChange}
               />
             </div>
 
---
+++ src/redux/actioncreators/auth.actioncreator.js
@@ -0,0 +1,3 @@
+export const StartLogin = (email, password) => async () => {
+  console.log(email, password);
+};
